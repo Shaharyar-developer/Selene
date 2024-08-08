0a6e01f051dd44801ac37cdbbff4a7e8d764fe59
Shaharyar-developer <Shaharyar-developer@users.noreply.github.com>
2024-08-08 22:41:54 +0000

    deploy: 85b4728b5ffeccaa00aaa87bdd845f4e51dbfa38

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA3XOQQqAIBQE0LvM2gpaepWIMPygpCn6iyC6e4uiRdZ2hjdMt4NpY0ioaCujZu0oQSAqNpDwQS+OcqOiHe6yNuwdBCY7a8j2EM+CpjxxiKW+i3/pw2gdlfDK/12mtH7dvfK360/vqkeh7QAAAA=="
